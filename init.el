16fcff2ba4b2b871286b977230c6b684f728b98e
einsteiner123 <einsteiner123@gmail.com>
2013-03-12 06:56:20 +0800

    me

--- init.el
+++ init.el
@@ -91,5 +91,8 @@
 (load "~/.emacs.d/key-bind.el")
 (load "~/.emacs.d/xcscope.el")
 ;(load "~/.emacs.d/auto-insert.el")
-(load "~/.emacs.d/c-style.el")
-
+;(load "~/.emacs.d/c-style.el")
+;(load "~/.emacs.d/zencoding/zencoding-mode.el")
+;;(add-to-list "~/emacs.d/zencoding")
+;;(require 'zencoding-mode)
+;;(add-hook 'sgml-mode-hook 'zencoding-mode)
